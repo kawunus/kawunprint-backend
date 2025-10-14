43622c3a0e3c363a4ee7b17bf6faed1eece524f6
stanislav <s.markovtsev@gmail.com>
2025-10-15 00:21:37 +0300

    rename MaterialTable.kt to FilamentTypeTable.kt

--- src/main/kotlin/data/model/tables/FilamentTypeTable.kt
+++ src/main/kotlin/data/model/tables/FilamentTypeTable.kt
@@ -3,7 +3,7 @@ package su.kawunprint.data.model.tables
 import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.Table
 
-object MaterialTable: Table() {
+object FilamentTypeTable: Table() {
     val id: Column<Int> = integer(name = "id").autoIncrement()
     val name: Column<String> = varchar(name = "name", length = 30)
 }
